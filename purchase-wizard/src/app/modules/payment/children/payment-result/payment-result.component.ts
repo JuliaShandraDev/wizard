6c01629361ceda266304f1cedb4cf97667b483e8
JuliaShandraDev <julia.shandra.dev@gmail.com>
2022-01-19 16:36:14 +0200

    add bootstrap styles

--- purchase-wizard/src/app/modules/payment/children/payment-result/payment-result.component.ts
+++ purchase-wizard/src/app/modules/payment/children/payment-result/payment-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-payment-result',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentResultComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+  viewResult(): void {
+    this.router.navigate(['/'])
   }
 
 }
